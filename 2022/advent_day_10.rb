31348b49b0b942fb3c0faf93f0304c1a73af256a
Jeremiah White <jwhite@hbu.co>
2022-12-10 01:28:39 -0600

    day 10 cleaned up

--- 2022/advent_day_10.rb
+++ 2022/advent_day_10.rb
@@ -3,47 +3,38 @@ file = File.open('inputs/2022_10.txt')
 
 file_data = file.readlines.map(&:chomp)
 
+def div_forty?(num)
+  (num % 40).zero?
+end
+
+def sprite_vis?(row_i, addx)
+  (row_i - addx).abs <=1
+end
+
+def reset_row(row)
+  puts row
+  ''
+end
+
 cycle = 0
 addx = 1
 sig_str = []
 row = ''
 
 file_data.each do |ins|
-  if ins == 'noop'
-    row += ((cycle % 40) - addx).abs <=1 ? '#' : '.'
+  (1..ins.split.size).each do |i|
+    row += sprite_vis?(cycle % 40, addx) ? '#' : '.'
+
     cycle += 1
 
-    if cycle % 40 == 0
-      p row
-      row = ''
-    end
+    row = reset_row(row) if div_forty?(cycle)
 
-    sig_str.push(addx * cycle) if (cycle + 20) % 40 == 0
-  else
-    row += ((cycle % 40) - addx).abs <=1 ? '#' : '.'
-    cycle += 1
+    sig_str.push(addx * cycle) if div_forty?(cycle + 20)
 
-    if cycle % 40 == 0
-      p row
-      row = ''
-    end
-
-    sig_str.push(addx * cycle) if (cycle + 20) % 40 == 0
-
-    row += ((cycle % 40) - addx).abs <=1 ? '#' : '.'
-    cycle += 1
-
-    if cycle % 40 == 0
-      p row
-      row = ''
-    end
-
-    sig_str.push(addx * cycle) if (cycle + 20) % 40 == 0
-
-    addx += ins.split(' ').last.to_i
+    addx += ins.split(' ').last.to_i if i == 2
   end
 end
 
-p "Signal Strengths: #{sig_str.sum}"
+puts "\nSignal Strengths: #{sig_str.sum}"
 
 file.close
